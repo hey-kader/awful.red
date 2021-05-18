4a77a9bc9b65be833a63b97b78246b2ad846d911
pablopenrose <kaderarnold@icloud.com>
2021-05-18 16:32:52 -0400

    made the header-bannder fit in the same row as our ordered list in Head.css

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function About () {
     return (
         <div className="wrapper">
-            <h1>About</h1>
+            <h1 id="main-title-wrapper">About</h1>
         </div>
     )
 }
