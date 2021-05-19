09a3e60bafe643dd8063417da90668b8804f9e4b
pablopenrose <kaderarnold@icloud.com>
2021-05-18 22:39:23 -0400

    routine updates

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+require("../card.css");
+
 function About () {
     return (
         <div className="wrapper">
-            <h1 id="main-title-wrapper">About</h1>
+            <div className="inner-wrapper">
+                <h1 id="main-title-wrapper">About</h1>
+                <br />
+                <br />
+                <br />
+                <br />
+            </div>
+            
+
+            <div id="web" className="card-wrapper">
+                <h2>web</h2>
+            </div>
+            <div id="gui" className="card-wrapper">
+                <h2>gui</h2>
+            </div>
+            
+            <div id="analog" className="card-wrapper">
+                <h2>analog</h2>
+            </div>
+            <div id="gallary" className="card-wrapper">
+                <h2>gallary</h2>
+            </div>
         </div>
     )
 }
