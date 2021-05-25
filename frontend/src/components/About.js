03d446dabb1069f06353f74bf19b1f99169b7bbd
pablopenrose <kaderarnold@icloud.com>
2021-05-25 01:27:37 -0400

    styling

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,7 +7,9 @@ function About () {
     return (
         <div className="wrapper">
            <h1 id="main-title-wrapper">About</h1>
-
+            <div className="about-content-wrapper">
+                <p>Adipisicing consequatur fugit modi ullam voluptas? Dolore saepe dolorum dolore corrupti aspernatur nesciunt, repellendus delectus assumenda Suscipit quibusdam corrupti voluptatum omnis voluptatibus similique. Sit pariatur autem harum rem illo. Mollitia laborum consequuntur neque vel et? Eveniet omnis rem eius quisquam voluptates blanditiis Fugiat suscipit unde obcaecati delectus veritatis. Incidunt omnis.</p>
+            </div>
         </div>
     )
 }
